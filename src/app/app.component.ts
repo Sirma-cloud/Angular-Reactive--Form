be9a990ded5004d240b836cb61bc6bcbc9fd61e9
Sirma-cloud <kevinkiplimo99@gmail.com>
2023-04-25 22:56:39 +0300

    Added custom validation to my form

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -16,8 +16,8 @@ export class AppComponent implements OnInit{
       // In a FormGroup we pass an object{}
       // We  specify the form controls as a key value pair eg firstname 
       personalData:new FormGroup({
-        firstname: new FormControl(null,Validators.required),
-        lastname: new FormControl(null,Validators.required),
+        firstname: new FormControl(null,[Validators.required, this.noSpaceAllowed]),
+        lastname: new FormControl(null,[Validators.required, this.noSpaceAllowed]),
         email: new FormControl(null,[Validators.required, Validators.email]),
       }),
       gender: new FormControl('other'),
@@ -42,5 +42,11 @@ export class AppComponent implements OnInit{
   // Adding a new skill
   addSkills(){
     (<FormArray>this.reactiveForm.get('skills')).push(new FormControl(null, Validators.required));
+  }
+  noSpaceAllowed(control: FormControl){
+    if(control.value != null && control.value.indexOf(' ') != -1){
+      return {noSpaceAllowed: true}
+    }
+    return null;
   }
 }
